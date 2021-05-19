59b3b5077c22fce6e232d967574742ccab1927f7
AsishMandoi <luckyasish13@gmail.com>
2021-05-19 07:50:50 +0530

    Initial commit

--- components/layout.js
+++ components/layout.js
@@ -60,6 +60,7 @@ export default function Layout({ children, home }) {
                     </>
                 )}
             </header>
+            
             <main>{children}</main>
             {!home && (
                 <div className={styles.backToHome}>
--- pages/index.js
+++ pages/index.js
@@ -8,6 +8,7 @@ export default function Home() {
       <Head>
         <title>{siteTitle}</title>
       </Head>
+      
       <section className={utilStyles.headingMd}>
         <p>Hello, I'm Asish. I'm a sophomore studying Electrical Engineering @IITK. You can contact me on <a href="https://www.facebook.com/asish.mandoi/">Facebook</a> or on <a href="https://www.linkedin.com/in/asish-mandoi-4178581b4/">LinkedIn</a>.</p>
         <p>Check out my <a href="https://github.com/AsishMandoi">GitHub profile</a>.</p>
